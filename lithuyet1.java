94fc545d706c5625e5a9b31001dab8c6014894de
nguyentay2004 <114301242+nguyenaty2004@users.noreply.github.com>
2023-02-20 08:53:56 +0700

    baitapbotuc

---
+++ lithuyet1.java
@@ -0,0 +1,7 @@
+package baitapbotuc;
+
+public class lithuyet1 {
+    public static void main(String[] args) {
+        System.out.println("Vòng lặp for trong java được sử dụng để lặp một phần của chương trình nhiều lần. Nếu số lần lặp là cố định thì vòng lặp for được khuyến khích sử dụng, còn nếu số lần lặp không cố định thì nên sử dụng vòng lặp while hoặc do while.khởi tạo biến");
+    }
+}
---
+++ lithuyet2.java
@@ -0,0 +1,9 @@
+package baitapbotuc;
+
+public class lithuyet2 {
+    public static void main(String[] args) {
+        System.out.println("b1 ta cần khai báo biến kiểm tra điều kiện và giá trị tăng/ giảm giá trị biến");
+
+
+    }
+}
