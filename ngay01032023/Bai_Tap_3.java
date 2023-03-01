cb0a0de691418b2707f51e25db52ac0d5bf7d372
nguyentay2004 <114301242+nguyenaty2004@users.noreply.github.com>
2023-03-01 16:54:18 +0700

    nopbaitap

--- ngay01032023/Bai_Tap_3.java
+++ ngay01032023/Bai_Tap_3.java
@@ -1,2 +1,17 @@
 package baitapbotuc.ngay01032023;
 
+public class Bai_Tap_3 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i += 2) {
+            if (array[i] == 5) {
+                continue;
+            }
+            sum = sum + array[i];
+
+        }
+        System.out.print("Tong vi tri 1 3 7 9 : " + sum);
+    }
+}
