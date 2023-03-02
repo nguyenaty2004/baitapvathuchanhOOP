edf1fa6f69341d6014da36e7c9a77d96868dd9b9
nguyentay2004 <114301242+nguyenaty2004@users.noreply.github.com>
2023-03-02 11:45:08 +0700

    Nopbaitap

--- ngay01032023/Peron.java
+++ ngay01032023/Peron.java
@@ -1,2 +1,25 @@
-package baitapbotuc.ngay01032023;public class Peron {
+package baitapbotuc.ngay01032023;
+
+public class Peron {
+    String name;
+    String gender;
+    int age;
+    String clasName;
+
+    public String getInf(){
+        return "name - " + name + "\ngender - " + gender + "\nage - " + age + "\nclaname - " + clasName;
+
+
+    }
+
+    public static void main(String[] args) {
+        Peron  person1 = new Peron ();
+
+        person1.name = "Nguyen Tay";
+        person1.gender = "Nam";
+        person1.age = 18;
+        person1.clasName = "IBSK1D2";
+
+        System.out.println(person1.getInf());
+    }
 }
